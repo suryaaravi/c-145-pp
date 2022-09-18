f42dfcaae0c4474ee1be4988af44e6830aae6ecb
suryaaravi <102033637+suryaaravi@users.noreply.github.com>
2022-09-18 14:10:54 +0530

    Add files via upload

---
+++ Student_Driving_license.py
@@ -0,0 +1,44 @@
+from tkinter import*
+
+root=Tk()
+root.title("student_Driving_license")
+root.geometry("400x250")
+
+root.configure(bg="white")
+canvas=Canvas(root,width=400,height=250)
+canvas.create_image(0,0,anchor=NW)
+canvas.create_rectangle(0, 0, 400, 45, fill="#F6091E")
+
+
+label_heading = canvas.create_text(200,20, font=('Times', '24', 'bold italic') ,text="Driving License", fill = "white")
+label_id_tag = canvas.create_text(25,60, font=('Times', '14', 'bold') ,text="ID :")
+label_name_tag = canvas.create_text(30,100, font=('Times', '12', 'bold') ,text="Name :")
+label_dob_tag = canvas.create_text(52,120, font=('Times', '12', 'bold') ,text="Date of birth :")
+label_pin_tag = canvas.create_text(32,140, font=('Times', '12', 'bold') ,text="Pin no. :")
+label_address_tag = canvas.create_text(36,160, font=('Times', '12', 'bold') ,text="Address :")
+label_vehicle_type_tag = canvas.create_text(155,180, font=('Times', '12', 'bold') ,text="Authorisation to drive the following vehicles :")
+
+label_id=Label(root,text=["19827198349"])
+label_name=Label(root,text=["Black pink"])
+label_dob=Label(root,text=["8 August 2000"])
+label_pin=Label(root,text=["451478"])
+label_address=Label(root,text=["Seoul Forest Trimage"])
+label_vehicle_type=Label(root,text=["Two Four"])
+
+# Define the function
+
+    
+    
+button1 = Button(root,text=["show my driving lisence"]) 
+
+button1.configure(width = 20 , activebackground = "#9EC6EE", relief= FLAT)
+button1_window = canvas.create_window(200, 235, anchor=CENTER, window=button1)
+label_id_window = canvas.create_window(80, 60, anchor=CENTER, window=label_id)
+label_name_window = canvas.create_window(100, 100, anchor=CENTER, window=label_name)
+label_dob_window = canvas.create_window(140, 120, anchor=CENTER, window=label_dob)
+label_pin_window = canvas.create_window(85, 140, anchor=CENTER, window=label_pin)
+label_address_window = canvas.create_window(130, 160, anchor=CENTER, window=label_address)
+label_vehicle_no_window = canvas.create_window(335, 180, anchor=CENTER, window=label_vehicle_type)
+canvas.pack()
+
+root.mainloop()
